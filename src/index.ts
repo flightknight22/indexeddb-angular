882720625f783941c44feb7160029030bf7a4585
Avery knight <knightav@mnstate.edu>
2018-03-15 09:29:20 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ export class IndexedDBAngular {
                 let request = this.utils.indexedDB.open(this.dbWrapper.dbName, version);
                 request.onsuccess = function (e) {
                     self.dbWrapper.db = request.result;
-                    resolve();
+                    resolve(request.result);
                 };
 
                 request.onerror = function (e) {
